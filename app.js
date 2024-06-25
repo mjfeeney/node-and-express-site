04d941ee7bd3fb5a037922df135f1f7e4dc9995f
mjfeeney <mjfeeney3@gmail.com>
2024-06-25 17:15:41 -0400

    Small start
    After installing express and pug and dependencies, updated app.js with the first routes.
    
    Created images folder for project screenshots.
    
    Created data.json file for testing to start with, formatted based of Step #3 of project instructions.

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'pug');
+
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.listen(3000, () => {
+  console.log("The app is running on localhost:3000")
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

786a0f3b040bf7a20f1e5bfe3002193f3f63de99
mjfeeney <mjfeeney3@gmail.com>
2024-06-26 22:59:06 -0400

    Static Assets and updates
    Set up images folder and included them in the express app along with the other static assets (css, js).
    
    Completed all project data entry into the data.json file.
    
    Confirmed styles and page layout of index page are working and data is synced up properly.

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
 const express = require('express');
+const data = require('./data.json');
+
+
 const app = express();
 
+
 app.set('view engine', 'pug');
+app.use('/static', express.static('public'));
 
 
-app.get('/', (req, res) => {
-  res.render('index');
+
+// GLOBAL things
+app.use((req, res, next) => {
+  res.locals.profilePic = '/static/images/profile.png';
+  next();
 });
 
+// INDEX page things
+app.get('/', (req, res) => {
+   // res.locals
+  const projects = data.projects;
+ 
+  res.render('index', { 
+    testText: "Testing Data from data.json",
+    projectName: projects[3].project_name,
+    projectDesc: projects[3].description,
+    projectImage1: projects[3].image_urls[0]
+  });
+});
+
+// ABOUT page things
 app.get('/about', (req, res) => {
   res.render('about');
 });
+
+
 
 app.listen(3000, () => {
   console.log("The app is running on localhost:3000")
